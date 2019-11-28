38a0e40e31a7bae6c2acda559b1a4c1f0184e7d6
Sebastian Schmidt <publicarray@users.noreply.github.com>
2019-11-28 20:50:01 +1100

    Add index.html
    Offline paper lottery draw

--- src/ran.js
+++ src/ran.js
@@ -6,14 +6,14 @@ const MAX_NUMBER = 100
 async function handleRequest(request) {
     let parameters = new URL(request.url).searchParams
     let maxNumber = parameters.get('max') || MAX_NUMBER
-    return new Response(secureMathRandom(maxNumber), {
+    return new Response(randomNumber(maxNumber), {
         headers: {
             'content-type': 'text/plain',
         },
     })
 }
 
-function secureMathRandom(max) {
+function randomNumber(max) {
     // Divide a random UInt32 by the maximum value (2^32 -1) to get a result between 0 and 1
     return Math.floor(crypto.getRandomValues(new Uint32Array(1))[0] / 4294967295 * max) + 1
 }
